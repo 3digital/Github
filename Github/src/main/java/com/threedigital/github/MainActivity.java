0b87e026429b86c7bdc801d30f5391e0900e7f6f
Devian Simmons <devians80@gmail.com>
2013-09-06 15:58:44 -0500

    Build directory not needed.

--- Github/src/main/java/com/threedigital/github/MainActivity.java
+++ Github/src/main/java/com/threedigital/github/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("Hack", "This is a message for commit changes");
+        Log.e("Hack", "This is a message for commit");
     }
 
 
