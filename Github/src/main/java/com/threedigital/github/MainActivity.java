14c61a36ddf7b2b284f2d48ac1dd68149d5332ff
Devian Simmons <devians80@gmail.com>
2013-09-06 15:02:03 -0500

    Initial commit

---
+++ Github/src/main/java/com/threedigital/github/MainActivity.java
@@ -0,0 +1,26 @@
+package com.threedigital.github;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.e("Hack", "This is a message for commit changes");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
